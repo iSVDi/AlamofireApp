69106937846e64dd70330d1ac5a35a43704fa0cb
Daniil Shestopalov <d.shestopalov@letmecode.org>
2024-12-11 09:41:54 +0300

    create project

---
+++ AlamofireApp/AlamofireAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlamofireAppApp.swift
+//  AlamofireApp
+//
+//  Created by Daniil on 11.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct AlamofireAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
